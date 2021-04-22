24e0cdc1832f8edc6bcbb059669229abf3a48d64
sim <3246917558@qq.com>
2021-04-22 10:55:12 +0800

    Big bad egg optimizes the some codes, please read carefully.

--- src/main/java/com/sim/sys/controller/AdminController.java
+++ src/main/java/com/sim/sys/controller/AdminController.java
@@ -3,10 +3,13 @@ package com.sim.sys.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.sim.sys.entity.Admin;
+import com.sim.sys.entity.Entering;
 import com.sim.sys.service.impl.AdminServiceImpl;
 
 
+import com.sim.sys.service.impl.EnteringServiceImpl;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -25,6 +28,9 @@ public class AdminController {
     @Resource
     private AdminServiceImpl adminService;
 
+    @Resource
+    private EnteringServiceImpl enteringService;
+
     /**
      * 登陆
      * @param account
@@ -36,6 +42,37 @@ public class AdminController {
     public String  verifyUserSignIn(@RequestParam String account, @RequestParam String password){
         return JSON.toJSONString(adminService.verifyUser(new Admin(account, password)));
     }
+
+    @PostMapping("/entering/insertEntering")
+    @ApiOperation(value = "登记入库信息")
+    public String insertEntering(@RequestBody Entering entering){
+        if(enteringService.insert(entering)!=null){
+            System.out.println("入库登记成功");
+            return "ok";
+        }
+        return "no";
+    }
+
+    @PostMapping("/entering/selectEntering")
+    @ApiOperation(value = "查询入库信息")
+    public List<Entering> selectLimitEntering(@RequestParam int offset,@RequestParam int limit){
+        if(enteringService.queryAllByLimit(offset,limit)!=null){
+            System.out.println("查询成功");
+            return enteringService.queryAllByLimit(offset,limit);
+        }
+        return null;
+    }
+
+    @PostMapping("/entering/updateEnteringById")
+    @ApiOperation(value = "更新入库信息")
+    public Entering updateEnteringById(@RequestBody Entering entering){
+        if(enteringService.update(entering)!=null){
+            System.out.println("修改成功");
+            return enteringService.update(entering);
+        }
+        return null;
+    }
+
 
 
 
